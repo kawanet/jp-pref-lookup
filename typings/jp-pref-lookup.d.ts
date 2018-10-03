b703fcbc3c15c0e47172caec640d7990f0f33139
Yusuke Kawasaki <u-suke@kawa.net>
2018-10-03 15:43:23 +0900

    npm run tsc-typings

--- typings/jp-pref-lookup.d.ts
+++ typings/jp-pref-lookup.d.ts
@@ -1,11 +1,12 @@
+export interface LookupOptions {
+    ll?: string;
+    lat?: number;
+    lng?: number;
+    mesh?: string;
+}
 export declare module Pref {
     function all(): Array<string>;
     function code(name: string): string;
-    function name(code: any): string;
-    function lookup(options: {
-        ll?: string;
-        lat?: number;
-        lng?: number;
-        mesh?: string;
-    }): string[];
+    function name(code: string | number): string;
+    function lookup(options?: LookupOptions): string[] | undefined;
 }
